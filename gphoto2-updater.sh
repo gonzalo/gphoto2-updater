ad48643fd031bf7abdbbfb72ed64a219586979ff
Gonzalo Cao Cabeza de Vaca <gonzalo.cao@gmail.com>
2013-01-18 10:16:28 +0100

    First script version. Functional over a clean Raspbian installation.

---
+++ gphoto2-updater.sh
@@ -0,0 +1,152 @@
+#/bin/sh
+
+# Gphoto2 2.5.0 compiler and installer script v0.1
+#
+# This script is specifically created for Raspbian http://www.raspbian.org
+# and Raspberry Pi http://www.raspberrypi.org but should work over any 
+# Debian-based distribution
+
+# Created and mantained by Gonzalo Cao Cabeza de Vaca
+# Please send any feedback or comments to gonzalo.cao(at)gmail.com
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+if [ "$(whoami)" != "root" ]; then
+	echo "Sorry, this script must be executed with sudo or as root"
+	exit 1
+fi
+
+echo 
+echo "----------------"
+echo "Updating sources"
+echo "----------------"
+echo
+
+apt-get update
+
+echo 
+echo "--------------------------"
+echo "Removing gphoto2 if exists"
+echo "--------------------------"
+echo
+
+apt-get remove -y gphoto2
+
+echo 
+echo "-----------------------"
+echo "Installing dependencies"
+echo "-----------------------"
+echo
+
+apt-get install -y libltdl-dev libusb-dev libexif-dev libpopt-dev
+
+echo 
+echo "-------------------------"
+echo "Creating temporary folder"
+echo "-------------------------"
+echo
+
+mkdir gphoto2-temp-folder
+cd gphoto2-temp-folder
+
+echo 
+echo "-------------------------"
+echo "Downloading libusb 1.0.11"
+echo "-------------------------"
+echo
+
+wget http://ftp.de.debian.org/debian/pool/main/libu/libusbx/libusbx_1.0.11.orig.tar.bz2
+tar xjvf libusbx_1.0.11.orig.tar.bz2
+cd libusbx-1.0.11/
+
+echo 
+echo "--------------------------------------"
+echo "Compiling and installing libusb 1.0.11"
+echo "--------------------------------------"
+
+./configure
+make
+make install
+cd ..
+
+
+echo 
+echo "-------------------------"
+echo "Downloading libusb 1.0.11"
+echo "-------------------------"
+echo
+
+wget http://downloads.sourceforge.net/project/gphoto/libgphoto/2.5.0/libgphoto2-2.5.0.tar.bz2
+tar xjf libgphoto2-2.5.0.tar.bz2
+cd libgphoto2-2.5.0
+
+echo 
+echo "-----------------------------------"
+echo "Compiling and installing libgphoto2"
+echo "-----------------------------------"
+echo
+
+./configure
+make
+make install
+cd ..
+
+echo 
+echo "-------------------------"
+echo "Downloading gphoto2 2.5.0"
+echo "-------------------------"
+echo
+
+wget http://downloads.sourceforge.net/project/gphoto/gphoto/2.5.0/gphoto2-2.5.0.tar.gz
+tar xzvf gphoto2-2.5.0.tar.gz
+cd gphoto2-2.5.0
+
+echo 
+echo "--------------------------------"
+echo "Compiling and installing gphoto2"
+echo "--------------------------------" 
+echo
+
+./configure
+make
+make install
+cd ..
+
+echo 
+echo "-----------------"
+echo "Linking libraries"
+echo "-----------------"  
+echo
+
+ldconfig
+
+echo 
+echo "-------------------"
+echo "Removing temp files"
+echo "-------------------"
+echo
+
+cd ..
+rm -r gphoto2-temp-folder
+
+
+
+echo 
+echo "--------------------"
+echo "Finished!! Enjoy it!"
+echo "--------------------"
+echo
+
+gphoto2 --version
